ff04fb26c8f73bc5e1927ebd21302db409041c4e
Warragal <kapnik36@gmail.com>
2019-05-14 22:15:38 +0300

    Beginning

---
+++ Navigator/app/src/main/java/com/example/navigator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.navigator;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng position = new LatLng(53.917521, 27.593342);
+        map.addMarker(new MarkerOptions().position(position).title("BSUIR"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+}
