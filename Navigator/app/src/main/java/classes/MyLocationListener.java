df008616fc843c33f192c449fe767299b32bf2c6
Warragal <kapnik36@gmail.com>
2019-05-20 21:28:46 +0300

    Trying to get permissions from the Android

--- Navigator/app/src/main/java/classes/MyLocationListener.java
+++ Navigator/app/src/main/java/classes/MyLocationListener.java
@@ -1,14 +1,19 @@
 package classes;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 public class MyLocationListener implements LocationListener {
 
     private double myLocationLongitude;
     private double myLocationLattitude;
+    private LocationManager locationManager;
+
 
     public MyLocationListener(double myLocationLattitude, double myLocationLongitude) {
         this.myLocationLongitude = myLocationLongitude;
@@ -20,6 +25,10 @@ public class MyLocationListener implements LocationListener {
         this.myLocationLattitude = 0;
     }
 
+    public void setLocationManager(LocationManager locationManager) {
+        this.locationManager = locationManager;
+    }
+
     public double getMyLocationLongitude() {
         return myLocationLongitude;
     }
@@ -28,20 +37,55 @@ public class MyLocationListener implements LocationListener {
         return myLocationLattitude;
     }
 
-    @Override
-    public void onLocationChanged(Location loc) {
-        if (loc.getProvider().equals(LocationManager.GPS_PROVIDER)) {
-            myLocationLongitude = loc.getLongitude();
-            myLocationLattitude = loc.getLatitude();
-        }
+    public void start(){
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, this);
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, this);
+        checkEnabled();
+    }
+
+    public void pause(){
+        locationManager.removeUpdates(this);
     }
 
     @Override
-    public void onProviderDisabled(String provider) {}
+    public void onLocationChanged(Location location) {
+        showLocation(location);
+    }
 
     @Override
-    public void onProviderEnabled(String provider) {}
+    public void onProviderDisabled(String provider) {
+        checkEnabled();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        checkEnabled();
+        //showLocation(locationManager.getLastKnownLocation(provider));
+    }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            myLocationLattitude = location.getLatitude();
+            myLocationLongitude = location.getLongitude();
+        } else if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            myLocationLattitude = location.getLatitude();
+            myLocationLongitude = location.getLongitude();
+        }
+    }
+
+    public void checkEnabled() {
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
+        !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) return;
+    }
 }
--- Navigator/app/src/main/java/com/example/navigator/MainActivity.java
+++ Navigator/app/src/main/java/com/example/navigator/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.navigator;
 import classes.MyLocationListener;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
-import android.location.LocationListener;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,11 +20,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_LOCATION = 2;
     GoogleMap map;
-    //LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    //LocationListener locationListener = new MyLocationListener();
+    MyLocationListener list = new MyLocationListener();
+    private LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +38,119 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        list.setLocationManager(locationManager);
+//        list.start();  //изначально это должно было быть в отдельной функции, но там возникли проблемы с аргументом context
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+//        }
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, list);
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, list);
+
+        if(hasPermissions()) {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, list);
+//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, list);
+            list.checkEnabled();
+        }
+        else{
+            requestPerms();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         map = googleMap;
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
-        LatLng position = new LatLng(0,0);
+        LatLng position = new LatLng(0, 0);
         map.addMarker(new MarkerOptions().position(position).title("You here"));
         map.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+
+    private boolean hasPermissions() {
+        int res = 0;
+        //string array of permissions,
+        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+
+        for (String perms : permissions) {
+            res = checkCallingOrSelfPermission(perms);
+            if (!(res == PackageManager.PERMISSION_GRANTED)) {
+                return false;
+            }
+        }
+
+        permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
+        for (String perms : permissions) {
+            res = checkCallingOrSelfPermission(perms);
+            if (!(res == PackageManager.PERMISSION_GRANTED)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void requestPerms() {
+        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, REQUEST_LOCATION);
+        }
+        permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean allowed = true;
+
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+
+                for (int res : grantResults) {
+                    // if user granted all permissions.
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+                }
+
+                break;
+            default:
+                // if user not granted permissions.
+                allowed = false;
+                break;
+        }
+
+        if (allowed) {
+            //user granted all permissions we can perform our task.
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, list);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, list);
+        } else {
+            // we will give warning to user that they haven't granted permissions.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "Storage Permissions denied.", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //showNoStoragePermissionSnackbar();
+                }
+            }
+        }
     }
 }
